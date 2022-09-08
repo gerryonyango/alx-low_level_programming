6e9d09378b41c825cb050782b094c0770189453d
Gerry Onyango <gerryjayson@gmail.com>
2022-09-08 06:43:11 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: We are noy allowed to use printf so must use put to print this string
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
