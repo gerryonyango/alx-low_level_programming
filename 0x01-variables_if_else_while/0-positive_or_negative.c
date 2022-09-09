c6e65fcda3576695944408b946356e371c092c81
Gerry Onyango <gerryjayson@gmail.com>
2022-09-09 06:55:39 -0700

    Betty Coding style Curly Braces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0){
-		printf("%i is positive\n", n);}
-	else if (n == 0){
-		printf("%i is zero\n", n);}
-	else{
-		printf("%i is negative\n", n);}
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else
+	{
+		printf("%i is negative\n", n);
+	}
+
  	return (0);
- }
+ 
+}
