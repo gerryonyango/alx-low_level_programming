6d930503afe7afb860d6162d61080f8b48316784
gerryonyango <gonyango18@gmail.com>
2023-09-25 06:34:29 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+
+/**
+ * free_listint - frees a list
+ * @head: the head of the list
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
